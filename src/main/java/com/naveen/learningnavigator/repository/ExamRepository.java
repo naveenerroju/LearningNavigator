f566f838ce1333cfea40d0fbcc717865eaf06a10
Naveen Kumar <naveenkumarerroju@gmail.com>
2024-09-18 16:52:25 +0530

    model mapper and repository primary key data type fixed

--- src/main/java/com/naveen/learningnavigator/repository/ExamRepository.java
+++ src/main/java/com/naveen/learningnavigator/repository/ExamRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExamRepository extends JpaRepository<Exam, Integer> {
+public interface ExamRepository extends JpaRepository<Exam, Long> {
 }
